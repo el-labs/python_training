66f90ce81467e2b3e16434e9a91a1dcc2c8c2f9c
el-labs <a.kosmatykh@hotmail.com>
2020-05-03 20:32:25 +0300

    Задание 2, вспомогательный класс

--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from group import Group
 import unittest
 
 
@@ -19,7 +20,7 @@ class UntitledTestCase(unittest.TestCase):
         self.login(driver, login="admin", password="secret")
         driver.find_element_by_xpath("//input[@value='Login']").click()
         driver.find_element_by_link_text("groups").click()
-        self.create_group(driver, name="Last", header="Last of as", footer="Last of nas")
+        self.create_group(driver, Group(name="Last", header="Last of as", footer="Last of nas"))
         driver.find_element_by_link_text("group page").click()
         driver.find_element_by_link_text("groups").click()
         driver.find_element_by_link_text("Logout").click()
@@ -32,17 +33,17 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys(password)
 
-    def create_group(self, driver, name, header, footer):
+    def create_group(self, driver, group):
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(name)
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(header)
+        driver.find_element_by_name("group_header").send_keys(group.header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(footer)
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
         driver.find_element_by_name("submit").click()
 
     def is_element_present(self, how, what):
