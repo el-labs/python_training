6652cbc585ce70a3a7255c26bcf377a726000d2c
el-labs <a.kosmatykh@hotmail.com>
2020-06-14 15:24:04 +0300

    ex 6 add test from group and contact

--- test/group.py
+++ test/group.py
@@ -16,6 +16,12 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("new").click()
+        self.fill_group(group)
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -25,23 +31,13 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def change_form(self, group):
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group(group)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
--- test/test_change_contact.py
+++ test/test_change_contact.py
@@ -1,10 +1,10 @@
 from model.сontact import Contact
 
 
-def test_add_contact(app):
+def test_change_contact(app):
     app.session.login(login="admin", password="secret")
-    app.contact.create_contact(Contact(firstname="1234", middlename="123123", lastname="12313",
-                               nickname="1231", title="1323", company="3123", address="1312",
-                               home="132", mobile="13", work="312", fax="13",
-                               email="13"))
+    app.contact.change_contact(Contact(firstname="mmmm", middlename="mmmmm", lastname="mmmmm",
+                               nickname="mmmmm", title="mmmmm", company="mmm", address="mmmm",
+                               home="mmmmmm", mobile="mmmmm", work="mmmmmm", fax="mmmm",
+                               email="mmmmmmmmm"))
     app.session.logout()
