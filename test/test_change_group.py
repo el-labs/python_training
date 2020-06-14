211d89ff83ec0538bbec2f5f30ac26d079b7ca02
el-labs <a.kosmatykh@hotmail.com>
2020-06-14 20:39:57 +0300

    ex 11 Сравнение списков групп улучшенная

--- test/test_change_group.py
+++ test/test_change_group.py
@@ -6,6 +6,10 @@ def test_change_group(app):
     if app.group.count() ==0:
         app.group.create(Group(name="", header="", footer=""))
     old_groups = app.group.get_group_list()
-    app.group.change_form(Group(name="Last1", header="Last of as1", footer="Last of nas1"))
+    group = Group(name="Last1", header="Last of as1", footer="Last of nas1")
+    group.id = old_groups[0].id
+    app.group.change_form(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
