3444b21bb8b69f7238739d34d80071b183852637
el-labs <a.kosmatykh@hotmail.com>
2020-07-02 00:00:01 +0300

    ex 16 fin

--- conftest.py
+++ conftest.py
@@ -1,17 +1,20 @@
 import pytest
 from fixture.application import Application
 
+
 fixture = None
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
-        if not fixture.is_valid:
-            fixture = Application()
+        if not fixture.is_valid():
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(login="admin", password="secret")
     return fixture
 
@@ -21,6 +24,9 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
-    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -6,24 +6,19 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
-    def open_home_page(self):
-        wd = self.wd
-        if wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//form[@name='MainForm']//div[1]//input[1]")) > 0:
-            return
-        wd.get("http://localhost/addressbook/")
-
-    def return_to_homepage(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def destroy(self):
-        self.wd.quit()
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -31,3 +26,14 @@ class Application:
             return True
         except:
             return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def return_to_homepage(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home").click()
+
+    def destroy(self):
+        self.wd.quit()
